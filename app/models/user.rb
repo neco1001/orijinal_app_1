1952840afe7a19868a8e119ceab00dcf6965346c
Kenichi Kaneko <k1001kenichi@gmail.com>
2019-02-15 13:49:08 +0900

    modeling-users終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, length: { maximum: 255 }
 end
